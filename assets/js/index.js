b3840143200a317141640178c62c70a07694ac42
Dev avology <devavology12@gmail.com>
2024-07-19 20:30:05 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,3 +14,22 @@ $(document).ready(function () {
     // $('.subpage-exp-content').slideToggle('slow');
     $(this).text($(this).text() == 'Read Less' ? 'Read More' : 'Read Less');
     });
+
+
+
+    $(document).ready(function () {
+      //toggle the component with class accordion_body
+      $(".accordion_head").click(function () {
+          if ($('.accordion_body').is(':visible')) {
+              $(".accordion_body").slideUp(300);
+              $(".plusminus").text('+');
+          }
+          if ($(this).next(".accordion_body").is(':visible')) {
+              $(this).next(".accordion_body").slideUp(300);
+              $(this).children(".plusminus").text('+');
+          } else {
+              $(this).next(".accordion_body").slideDown(300);
+              $(this).children(".plusminus").text('-');
+          }
+      });
+  });

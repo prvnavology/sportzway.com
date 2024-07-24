f4d5cadf4d88576c37eb64d2dd5242211de22908
Dev avology <devavology12@gmail.com>
2024-07-24 18:43:47 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -46,3 +46,54 @@ $(document).ready(function () {
           }
       });
   });
+
+
+  // //Cookies JS Start
+function SetCookie(cookieName, cookieValue, nDays) {
+  "use strict";
+  var today = new Date();
+  var expire = new Date();
+  if (nDays == null || nDays == 0) nDays = 1;
+  expire.setTime(today.getTime() + 3600000 * 24 * nDays);
+  document.cookie =
+    cookieName +
+    "=" +
+    escape(cookieValue) +
+    ";expires=" +
+    expire.toGMTString() +
+    "; path=/";
+}
+function ReadCookie(cookieName) {
+  "use strict";
+  var theCookie = " " + document.cookie;
+  var ind = theCookie.indexOf(" " + cookieName + "=");
+  if (ind == -1) ind = theCookie.indexOf(";" + cookieName + "=");
+  if (ind == -1 || cookieName == "") return "";
+  var ind1 = theCookie.indexOf(";", ind + 1);
+  if (ind1 == -1) ind1 = theCookie.length;
+  return unescape(theCookie.substring(ind + cookieName.length + 2, ind1));
+}
+function DeleteCookie(cookieName) {
+  "use strict";
+  var today = new Date();
+  var expire = new Date() - 30;
+  expire.setTime(today.getTime() - 3600000 * 24 * 90);
+  document.cookie =
+    cookieName + "=" + escape(cookieValue) + ";expires=" + expire.toGMTString();
+}
+function AcceptCookies() {
+  SetCookie("sgCookies", true, 30);
+  jQuery("#cookie-bar").hide();
+}
+$(document).ready(function (e) {
+  var toTop = $(
+    "<div id='cookie-bar'><div id='contentcookie'><p>We use cookies and similar technologies to recognize your repeat visits and preferences, to measure the effectiveness of campaigns, and improve our websites. For settings and more information about cookies, view our <a href='#'>Cookie Policy</a>. By clicking “I accept” on this banner or using our site, you consent to the use of cookies.</p><button id='Cookie' tabindex=1 onclick='AcceptCookies();'>Accept </button><div class='clear'></div></div></div>"
+  ).appendTo("body");
+  if (!ReadCookie("sgCookies")) {
+    //If the cookie has not been set
+    jQuery("#cookie-bar").show();
+  } else {
+    jQuery("#cookie-bar").hide();
+  }
+});
+// //Cookies JS Start
